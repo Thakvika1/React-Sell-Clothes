b655e989f83221cc540e0a7ce04317965afa26d8
Thakvika1 <thakvika.thorn@gmail.com>
2025-03-26 15:03:22 +0700

    make some changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from './components/Navbar'
 import Tapbar from './components/Tapbar'
 import ProductCard from './components/ProductCard'
+import Logo from './components/Logo'
 
 function App() {
   return (
     <>
       <Navbar />
+      <Logo />
       <Tapbar />
       <ProductCard />
     </>
